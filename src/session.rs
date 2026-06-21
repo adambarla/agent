977f865c3971cb53fc186fbdaada309ac21d81e1
Adam Barla <adam.barla@epfl.ch>
2026-06-21 23:05:46 +0200

    chore: one session test

--- src/session.rs
+++ src/session.rs
@@ -1,4 +1,4 @@
-use std::vec::Vec;
+use std::{collections::HashSet, vec::Vec};
 
 #[derive(Clone, Debug)]
 pub enum MessageRole {
@@ -41,21 +41,58 @@ impl Event {
 }
 
 pub trait Session {
-    fn append(&mut self, event: &Event);
+    fn append(&mut self, event: &Event) -> bool;
     fn get_all_events(&self) -> &Vec<Event>;
 }
 
 #[derive(Default)]
 pub struct InMemorySession {
     events: Vec<Event>,
+    event_ids: HashSet<String>,
 }
 
 impl Session for InMemorySession {
-    fn append(&mut self, event: &Event) {
+    fn append(&mut self, event: &Event) -> bool {
+        if self.event_ids.contains(&event.id) {
+            return false;
+        }
         self.events.push(event.clone());
+        self.event_ids.insert(event.id.clone());
+        true
     }
 
     fn get_all_events(&self) -> &Vec<Event> {
         &self.events
     }
 }
+
+#[macro_export]
+macro_rules! test_session {
+    ($name:ident, $session:expr) => {
+        #[cfg(test)]
+        mod $name {
+            use super::*;
+            use anyhow::Result;
+
+            #[test]
+            fn test_lifecycle() -> Result<()> {
+                let mut session = $session;
+                assert!(session.get_all_events().is_empty());
+                let e1 = Event::new(EventPayload::Message {
+                    role: MessageRole::User,
+                    content: "Hello".to_string(),
+                    tool_calls: Vec::new(),
+                });
+                assert!(session.append(&e1.clone()));
+                assert!(session.get_all_events().len() == 1);
+
+                let was_appended = session.append(&e1);
+                assert!(!was_appended);
+                assert_eq!(session.get_all_events().len(), 1);
+                Ok(())
+            }
+        }
+    };
+}
+
+test_session!(in_memory_session, InMemorySession::default());
